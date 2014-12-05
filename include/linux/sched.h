598dc9d676199545624aaba6c6fe6ff487c4e0f1
ion-storm <defconoii@gmail.com>
2014-12-05 08:55:13 -0500

    remove duplicate line in sched.h

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1282,8 +1282,6 @@ struct task_struct {
 #endif
 	unsigned long atomic_flags; /* Flags needing atomic access. */
 
-	unsigned long atomic_flags; /* Flags needing atomic access. */
-
 	pid_t pid;
 	pid_t tgid;
 
